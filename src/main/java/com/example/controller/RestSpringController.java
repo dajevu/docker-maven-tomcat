ff22c0bb9eb98f9f04bda765d9c057d1ee67ece4
dajevu <jdavis@zazarie.com>
2015-01-27 15:12:56 -0700

    bug fix

--- src/main/java/com/example/controller/RestSpringController.java
+++ src/main/java/com/example/controller/RestSpringController.java
@@ -3,7 +3,6 @@ package com.example.controller;
 import com.example.dao.CustomerDAO;
 import com.example.dao.CustomerDAOImpl;
 import com.example.model.Customer;
-import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
